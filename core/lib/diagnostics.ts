ecb853b9baca578b1bf2efa73b677ca6da868975
Jatin Kadian <52458646+Risky-Shot@users.noreply.github.com>
2026-01-05 22:41:25 +0530

    Comment out error message for ENOENT
    Commented out error logging for process tree usage data.

--- core/lib/diagnostics.ts
+++ core/lib/diagnostics.ts
@@ -97,7 +97,7 @@ export const getProcessesData = async () => {
         });
     } catch (error) {
         if ((error as any).code = 'ENOENT') {
-            console.error('Failed to get processes tree usage data.');
+            //console.error('Failed to get processes tree usage data.');
             if (txEnv.isWindows) {
                 //console.error('This is probably because the `wmic` command is not available in your system.');
                 //console.error('If you are on Windows 11 or Windows Server 2025, you can enable it in the "Windows Features" settings.');
